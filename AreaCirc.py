83e2d70be5231d75cbb3d2a44d6f2f0a91a3e473
Marcello Santos <pojucan@gmx.com>
2019-07-30 09:32:12 -0300

    Upload on Plataform

---
+++ AreaCirc.py
@@ -0,0 +1,12 @@
+#Programa 02 - AreaCirc
+#ID: PyExe-AreaCirc
+#Funcao: Calculando a area da circunferencia
+#Autor: Marcello Pojucan Magaldi Santos
+#Data: 29/07/2019
+#IDE: Atom
+#Acesso: https://www.overleaf.com/project/5d3f96981be9e511e91c6234
+
+pi = 3.14 #Valor de Pi
+r = 4 #Raio da circunferencia
+A = pi*(r**2)
+print("O valor da area da circunferencia eh:", A, "u.a")
